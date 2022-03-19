33a4cbf78301dcc460c76f3cecb6d865e946fcfc
Luis Dominguez <l.dominguez.aldana@gmail.com>
2022-03-18 23:51:57 -0600

    Directory names to Upper Case

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -11,8 +11,8 @@ const CategoryItem = ({ category }) => {
         }}
       />
       <div className='category-body-container'>
-        <h2>{title}</h2>
-        <p>Shop Now</p>
+        <h2>{title.toUpperCase()}</h2>
+        <p>SHOP NOW</p>
       </div>
     </div>
   );
