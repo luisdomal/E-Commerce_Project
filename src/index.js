315099c846cd68e028a7c1b960a376488ae93c08
Luis Dominguez <l.dominguez.aldana@gmail.com>
2022-03-18 23:46:12 -0600

    Authentication completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 
-import './index.css';
-import App from './App';
+import "./index.scss";
 
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
 );
