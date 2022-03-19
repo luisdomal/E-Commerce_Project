a3ed9981f05995ae149fac062920d0ede1b2d55d
Luis Dominguez <l.dominguez.aldana@gmail.com>
2022-03-18 21:38:21 -0600

    Firebase setup, signin component added, firebase libraries and user db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,20 @@
 import {
-    signInWithGooglePopup,
-    createUserProfileDocument,
-  } from '../../utils/firebase/firebase.utils';
-  
-  const SignIn = () => {
-    const logGoogleUser = async () => {
-      const response = await signInWithGooglePopup();
-      createUserProfileDocument(response);
-    };
-  
-    return (
-      <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      </div>
-    );
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
-  
-  export default SignIn;
-  
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,16 +5,17 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCh5_iVaM0gaAFzkzRL_ftnEdxfHRb6eOY",
-    authDomain: "crown-clothing-db-fcf7f.firebaseapp.com",
-    projectId: "crown-clothing-db-fcf7f",
-    storageBucket: "crown-clothing-db-fcf7f.appspot.com",
-    messagingSenderId: "521024067373",
-    appId: "1:521024067373:web:231b102ceac0cba7f2e5a4",
-    measurementId: "G-HBQ353MY0F"
-  };
+  apiKey: "AIzaSyCh5_iVaM0gaAFzkzRL_ftnEdxfHRb6eOY",
+  authDomain: "crown-clothing-db-fcf7f.firebaseapp.com",
+  projectId: "crown-clothing-db-fcf7f",
+  storageBucket: "crown-clothing-db-fcf7f.appspot.com",
+  messagingSenderId: "521024067373",
+  appId: "1:521024067373:web:231b102ceac0cba7f2e5a4",
+  measurementId: "G-HBQ353MY0F"
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 
@@ -24,11 +25,30 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  console.log(userAuth);
-};
-
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
